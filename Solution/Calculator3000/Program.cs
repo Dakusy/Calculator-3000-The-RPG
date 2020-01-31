55e6d85e0e3f790bbf59a5102eb675e942e8ab85
Dakusy <dakusy@gmail.com>
2020-01-31 20:06:38 +0100

    create a little menu

--- Solution/Calculator3000/Program.cs
+++ Solution/Calculator3000/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace Calculator3000
 {
@@ -6,7 +7,50 @@ namespace Calculator3000
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int choice;
+            do
+            {
+                Console.Write(Menu());
+                choice = Convert.ToInt32(Console.ReadLine());
+                string result = MenuChoice(choice);
+                Console.WriteLine(result);
+            } while (choice != 3);
+
+            Console.ReadLine();
+
+        }
+
+        public static string Menu()
+        {
+            return " *****Menu***** \n"
+                   + " 1 - Create Character \n"
+                   + " 2 - Options \n"
+                   + " 3 - Exit Game \n"
+                   + " \n "
+                   + " Choice ? \n ";
+        }
+
+        public static string MenuChoice(int choice)
+        {
+            string result = "";
+
+            switch (choice)
+            {
+                case 1:
+                    result += " Character Creation In Progress ! ";
+                    break;
+                case 2:
+                    result += " Options In Progress !";
+                    break;
+                case 3:
+                    result += " Bye Bye !";
+                    break;
+                default:
+                    result += " Nice Try ! Try Again !";
+                    break;
+                
+            }
+            return result;
         }
     }
 }
